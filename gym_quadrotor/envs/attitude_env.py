c4b913e521583c870855673c92368f84267e31f1
ngc92 <ngc92@users.noreply.github.com>
2018-07-13 14:04:59 +0200

    small fixes

--- gym_quadrotor/envs/attitude_env.py
+++ gym_quadrotor/envs/attitude_env.py
@@ -12,6 +12,8 @@ class CopterStabilizeAttitudeEnv(QuadRotorEnvBase):
     def __init__(self):
         super().__init__()
         self._target_yaw = 0
+        self._error_target = 1 * np.pi / 180
+        self._correct_counter = 0
 
     def _step_copter(self, action: np.ndarray):
         attitude = self._state.attitude
@@ -21,16 +23,25 @@ class CopterStabilizeAttitudeEnv(QuadRotorEnvBase):
         reward = -angle_error
         self.limit_attitude(np.pi/4)
         self.ensure_fixed_position()
+        # check whether error is below bound for a given time interval
+        if angle_error < self._error_target * self._error_target:
+            self._correct_counter += 1
+            # after 1 second, win the episode
+            if self._correct_counter > 50:
+                return 1.0, True, {}
+        else:
+            self._correct_counter = 0
         return reward, False, {}
 
     def _get_state(self):
         s = self._state
         rate = angvel_to_euler(s.attitude, s.angular_velocity)
-        state = [s.attitude.roll, s.attitude.pitch, s.attitude.yaw, rate[0], rate[1], rate[2]]
+        state = [s.attitude.roll, s.attitude.pitch, s.attitude.yaw - self._target_yaw, rate[0], rate[1], rate[2]]
         return np.array(state)
 
     def _reset_copter(self):
         self.randomize_angle(20)
         self._target_yaw = self._state.attitude.yaw
         self._state.position[2] = 1
+        self._correct_counter = 0
 
