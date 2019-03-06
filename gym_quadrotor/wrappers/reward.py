38cb6f269dab400a32b73aed32397627fef0dea0
ngc92 <7938269+ngc92@users.noreply.github.com>
2019-03-06 12:39:19 +0100

    bugfix

--- gym_quadrotor/wrappers/reward.py
+++ gym_quadrotor/wrappers/reward.py
@@ -1,6 +1,6 @@
 import gym
 from gym_quadrotor.envs import QuadRotorEnvBase
-from gym_quadrotor.envs.reward import AttitudeReward
+from gym_quadrotor.envs.reward import AttitudeReward, _NoArgumentPassed
 
 
 class DiscourageLongEpisodesWrapper(gym.Wrapper):
@@ -8,8 +8,9 @@ class DiscourageLongEpisodesWrapper(gym.Wrapper):
         super().__init__(env)
 
 
-def modify_attitude_reward(env: gym.Env, angle_factor=None, angvel_factor=None, angle_error_transform=None,
-                           angvel_error_transform=None):
+def modify_attitude_reward(env: gym.Env, angle_factor=None, angvel_factor=None,
+                           angle_error_transform=_NoArgumentPassed,
+                           angvel_error_transform=_NoArgumentPassed):
         reward = env.unwrapped._attitude_reward  # type: AttitudeReward
         reward.update_parameters(
             angle_factor=angle_factor,
